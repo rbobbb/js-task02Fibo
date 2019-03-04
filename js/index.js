8b87d6804fc18fcf16db74fb2da3a518b3625576
Roman <roman.bobyk@gmail.com>
2019-03-04 11:59:44 +0000

    fibo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const n = parseInt(prompt('Enter number for Fibonacci'));
+
 function fib(n) {
   let a = 1,
     b = 1;
@@ -9,6 +11,55 @@ function fib(n) {
   return b;
 }
 
-alert( fib(3) ); 
-alert( fib(33) ); 
-alert( fib(333) ); 
+alert('Your Fibonacci number is ' + fib(n)); 
+
+
+
+
+
+// alert('Your Fibonacci number is ');
+
+
+// function fib(n) {
+//   let a = 1,
+//     b = 1;
+//   for (let i = 3; i <= n; i++) {
+//     let c = a + b;
+//     a = b;
+//     b = c;
+//   }
+//   return b;
+// }
+
+// alert( fib(3) ); 
+// alert( fib(33) ); 
+// alert( fib(333) ); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function fib(n) {
+//   let a = 1,
+//     b = 1;
+//   for (let i = 3; i <= n; i++) {
+//     let c = a + b;
+//     a = b;
+//     b = c;
+//   }
+//   return b;
+// }
+
+// alert( fib(3) ); 
+// alert( fib(33) ); 
+// alert( fib(333) ); 
