f47dc85c07750b4e6244f9dbd259a5897c7858b2
Roman <roman.bobyk@gmail.com>
2019-03-04 08:52:47 +0000

    function fib(n) {   var a = 1,     b = 1;   for (var i = 3; i <= n; i++) {     var c = a + b;     a = b;     b = c;   }   return b; }
    alert( fib(3) ); // 2
    alert( fib(7) ); // 13
    clear
    
    clear
    
    rbob:~/workspace (gh-pages) $ git add .

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function fib(n) {
+  let a = 1,
+    b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+alert( fib(3) ); 
+alert( fib(33) ); 
+alert( fib(333) ); 
